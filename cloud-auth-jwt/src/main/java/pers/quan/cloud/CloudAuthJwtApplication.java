f44dfefcf4eff68a90f8aed13d348d167407ddd2
Force-oneself <1044749290@qq.com>
2021-03-01 12:43:20 +0800

    add zuul aggregate swagger

--- cloud-auth-jwt/src/main/java/pers/quan/cloud/CloudAuthJwtApplication.java
+++ cloud-auth-jwt/src/main/java/pers/quan/cloud/CloudAuthJwtApplication.java
@@ -6,12 +6,14 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableScheduling
 @EnableFeignClients
 @EnableZuulProxy
+@EnableSwagger2
 public class CloudAuthJwtApplication {
 
     public static void main(String[] args) {
